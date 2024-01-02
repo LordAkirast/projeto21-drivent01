573440d8af2a384ada6b65872911b4a7a64a5aeb
LordAkirast <darkwarish@gmail.com>
2024-01-02 13:47:14 -0300

    update tests/helpers.ts

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -8,10 +8,12 @@ import { prisma } from '@/config';
 export async function cleanDb() {
   await prisma.address.deleteMany({});
   await prisma.enrollment.deleteMany({});
-  await prisma.event.deleteMany({});
   await prisma.session.deleteMany({});
   await prisma.user.deleteMany({});
+  await prisma.event.deleteMany({});
 }
+
+
 
 export async function generateValidToken(user?: User) {
   const incomingUser = user || (await createUser());
