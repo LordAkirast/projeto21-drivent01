298afbcfac04cce74e7952fe6c8d6c3d10fa047e
LordAkirast <darkwarish@gmail.com>
2024-02-11 01:29:10 -0300

    jest correction

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   transform: {
     '.+\\.ts$': 'ts-jest',
-  }, 
+  },
   testMatch: ['<rootDir>/tests/**/*.(test|spec).ts'],
   setupFiles: ['<rootDir>/tests/setup-envs.ts'],
   setupFilesAfterEnv: ['<rootDir>/tests/setup-files-after-env.ts'],
